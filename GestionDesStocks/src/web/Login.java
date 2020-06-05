6961ba556483d2f91d5cb92e41e5a2c70e764977
JabranAboumerouane <jabran.aboumerouane@promsocatc.net>
2020-06-05 19:07:50 +0200

    05062020

--- GestionDesStocks/src/web/Login.java
+++ GestionDesStocks/src/web/Login.java
@@ -6,22 +6,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "Login")
+@WebServlet(description = "MaServlet",name = "Login", value = {"/Login"})
 public class Login extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String name = request.getParameter("nom");
-        String password = request.getParameter("passw");
-        if (name == admin && password == passw){
 
-        }
-        else{
-            
-        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        PrintWriter pr = response.getWriter();
+        pr.println("voila");
+
+        String name = request.getParameter("nom");
+        String password = request.getParameter("passw");
+        System.out.println("in servlet - login: " + name + " pw: " + password);
+        if (name.equals("admin") && password.equals("passw") ){
+            response.sendRedirect(request.getContextPath() + "/jsp/page1.jsp");
+        }
+        else{
+            response.sendRedirect("index.jsp");
+        }
     }
 }
